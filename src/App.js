47f8492654f7287c7646ec743f2a6ea1ed08be3e
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-09-19 20:27:14 +0530

    learn and add routers(partial)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/home.component';
+import {Route, Switch} from 'react-router-dom';
+
+const hatsPage = () => (
+  <h1>This is Hats Page</h1>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={hatsPage}/>
+      </Switch>
+      {/* <HomePage /> */}
     </div>
   );
 }
