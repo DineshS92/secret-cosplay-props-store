17239c75541ce347e5e0dd177b06baebee576e96
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-09-23 16:30:28 +0530

    implemented sign up and connected sign in with google to frontend

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SigninSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDoc } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -21,9 +21,25 @@ class App extends React.Component {
 
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      console.log(user);
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // console.log(user);
+      // this.setState({ currentUser: user });
+      // createUserProfileDoc(user);
+      if(userAuth) {
+        const userRef = await createUserProfileDoc(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     })
   }
 
