ba632a535efad3994ff6e4488c2a8b10104fa9f3
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-09-25 19:21:12 +0530

    added checkout items

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import {createStructuredSelector} from 'reselect'
 import './App.css';
 import HomePage from './pages/homepage/home.component';
 import {Route, Switch, Redirect} from 'react-router-dom';
+
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SigninSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDoc } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
@@ -50,6 +53,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage}/>
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin'
             render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SigninSignUp />)} />
         </Switch>
